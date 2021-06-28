37ef4a16e805dbfef0f0a5ba679a825195e6e49d
adina-tik-tak <adina@tik-tak.co.il>
2021-06-28 11:14:17 +0300

    start

--- src/components/contact-details.vue
+++ src/components/contact-details.vue
@@ -2,15 +2,14 @@
 <div>
     <input name='fname' type='text' placeholder='first name'  v-model="fname"/>
     <input name='lname' type='text' placeholder='last name'  v-model="lname"/>
-    <keyValue></keyValue>
-    <button @click="ADD_CONTACT(fname,lname)" ></button>
+   
 </div>
 </template>
 
 
 <script>
 import { mapMutations } from 'vuex'
-import keyValue from './keyValue.vue'
+
 
 export default {
     data: function () {
@@ -24,7 +23,7 @@ export default {
      ...mapMutations(['ADD_CONTACT'])
   },
    
-  components: {keyValue}
+
 }
 </script>
 <style>
